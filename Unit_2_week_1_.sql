0dc447f774ce4784d1d5bd570411b2aa5e30f9d9
achilov89 <81782778+achilov89@users.noreply.github.com>
2021-04-15 20:05:05 -0400

    Update Unit_2_week_1_.sql

--- Unit_2_week_1_.sql
+++ Unit_2_week_1_.sql
@@ -8,3 +8,24 @@ order by 2 desc
 
 ---Answer:Bridgeport
 
+---What metro area has the zip code with the largest population? 
+
+select  metro_city, zip, max (population) as largest_poulation
+from public.census_metro_data 
+group by metro_city, zip
+order by 3 desc 
+---Answer: Houston 
+
+---What state has the most metro areas?
+select state, count (metro_city) as number_of_metro_areas
+from public.census_metro_data 
+group by state
+order by 2 desc 
+---Answer:CA
+
+---Which metro area has the largest proportion of people aged 70-97?
+select metro_city , max (population_age_75_79) as number_people_aged_70_79
+from public.census_metro_data
+group by metro_city 
+order by 2 desc
+---Answer: Phoenix 
